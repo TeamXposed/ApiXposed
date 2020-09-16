e9604780cb1328d431795bd55ef3136c8d82b9e1
caioenio <caioenio6@gmail.com>
2020-09-16 20:27:43 -0300

    Criação do Schema de usuário

---
+++ src/models/UsuarioModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const usuario = new Schema ({
+    
+    nome: {
+        type: String,
+        required: 'Campo Obrigatório'
+    },
+
+    telefone: {
+        type: String,
+        required: 'Campo Obrigatório'
+    },
+
+    email: {
+        type: String,
+        required: 'Campo Obrigatório'
+    },
+
+    senha: {
+        type: String,
+        required: 'Campo Obrigatório'
+    },
+
+    cpf: {
+        type: String,
+        required: 'Campo Obrigatório'
+    }
+})
+
+
