89527dd1ce02fc3cf2bc273f53f13f8b10094539
Laís Regina <lregina322@outlook.com>
2020-09-16 21:22:12 -0300

    Criacão de denuncia e sugestao

---
+++ src/models/denunciaModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const denuncias = new Schema ({
+    
+    nome: {
+        type: String,
+        required: 'Campo Obrigatório'
+    },
+
+    assunto: {
+        type: String,
+        required: 'Campo Obrigatório'
+    },
+
+    nick_denuniado: {
+        type: String,
+        required: 'Campo Obrigatório'
+    },
+
+    jogo: {
+        type: String,
+        required: 'Campo Obrigatório'
+    },
+
+    descricao: {
+        type: String,
+        required: 'Campo Obrigatório'
+    },
+
+    /*  ---- Pesquisar sobre anexo ----
+        anexo: {
+        type: String,
+        required: 'Campo Obrigatório'
+    },
+    */
+  
+})
---
+++ src/models/sugestaoModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const sugestao = new Schema ({
+    
+    nome: {
+        type: String,
+        required: 'Campo Obrigatório'
+    },
+
+    jogo: {
+        type: String,
+        required: 'Campo Obrigatório'
+    },
+
+    ano: {
+        type: Date,
+        required: 'Campo Obrigatório'
+    },
+
+    descricao: {
+        type: String,
+        required: 'Campo Obrigatório'
+    },
+  
+    comentario: {
+        type: String,
+        required: 'Campo Obrigatório'
+    }
+
+})
