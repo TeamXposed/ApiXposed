fe5384bd35acca212f29dd819497e93531eaa763
Vinícius Monteiro <47702719+Viniiciusmonterio@users.noreply.github.com>
2020-10-28 19:54:07 -0300

    alteração na model e controller da sugestão

--- src/controllers/sugestaoController.js
+++ src/controllers/sugestaoController.js
@@ -2,46 +2,46 @@ const sugestao = require('../models/sugestaoModel')
 
 // Criação das funcionalidades das sugestões
 
-exports.criar = (req,res) => {
-    const {  nome, jogo, ano, descricao, comentario} = req.body
-    let novoSugestao = new sugestao({ nome, jogo, ano, descricao, comentario})
-    novoSugestao.save((error, sugestao) =>{
-        if(error){
+exports.criar = (req, res) => {
+    const { comentario } = req.body
+    let novoSugestao = new sugestao({ comentario })
+    novoSugestao.save((error, sugestao) => {
+        if (error) {
             res.send(error);
         }
-        res.send('Sua sugestão foi criado com sucesso')
+        res.send('Sua sugestão foi criada com sucesso!')
     })
 }
 
-exports.listar = function(req,res) {
-    sugestao.find({}, function(error,sugestao) {
-        if(error){
+exports.listar = function (req, res) {
+    sugestao.find({}, function (error, sugestao) {
+        if (error) {
             res.send(error);
         }
         res.json(sugestao);
     })
 }
 
-exports.atualizar = (req,res) => {
-    sugestao.findOneAndUpdate({_id: req.params.id }, req.body,{new: true} ,(error, sugestao) =>{
-        if(error){
+exports.atualizar = (req, res) => {
+    sugestao.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }, (error, sugestao) => {
+        if (error) {
             res.send(error);
         }
         let resposta = {
-            mensagem: 'O Jogo foi atualizado!',
-            data : sugestao
+            mensagem: 'A sugestão foi atualizado!',
+            data: sugestao
         }
         res.send('Sua sugestão foi atualizada com sucesso')
     })
 }
 
-exports.deletar = (req,res) => {
-    sugestao.remove({_id: req.params.id}, (error,sugestao) => {
-        if(error){
+exports.deletar = (req, res) => {
+    sugestao.remove({ _id: req.params.id }, (error, sugestao) => {
+        if (error) {
             res.send(error);
         }
         let resposta = {
-            mensagem: 'Jogo deletado!',
+            mensagem: 'Sugestão deletada!',
             data: sugestao
         }
         res.send('Sua sugestão foi deletada com sucesso')
--- src/models/sugestaoModel.js
+++ src/models/sugestaoModel.js
@@ -1,32 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const sugestao = new Schema ({
-    
-    nome: {
-        type: String,
-        required: 'Campo Obrigatório',
-        trim: true
-    },
+const sugestao = new Schema({
 
-    jogo: {
-        type: String,
-        required: 'Campo Obrigatório',
-        trim: true
-    },
-
-    ano: {
-        type: String,
-        required: 'Campo Obrigatório',
-        trim: true
-    },
-
-    descricao: {
-        type: String,
-        required: 'Campo Obrigatório',
-        trim: true
-    },
-  
     comentario: {
         type: String,
         required: 'Campo Obrigatório',
